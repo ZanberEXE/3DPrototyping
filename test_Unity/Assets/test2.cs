f4b506e2575f3a3e5e50032d9927d5f6d2fc3956
Dr. Rabbit <huber.andreas123@gmail.com>
2018-11-03 15:24:30 +0100

    test

---
+++ test_Unity/Assets/test2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test2 : MonoBehaviour
+{
+    float moveSpeed;
+
+	// Use this for initialization
+	void Start ()
+    {
+        moveSpeed = 3f;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //if (Input.GetMouseButtonDown(0))
+        //{
+
+        //    transform.Translate(Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f);
+        //}
+        transform.Translate(Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime, 0f, Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime);
+	}
+}
