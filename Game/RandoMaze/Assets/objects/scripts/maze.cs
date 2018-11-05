efe2ba26a1c5e9a10bd940afe263ba185fac8ffc
Felix <felix.s96ss@gmail.com>
2018-11-05 16:17:59 +0100

    listidea

---
+++ Game/RandoMaze/Assets/objects/scripts/maze.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class maze : MonoBehaviour {
+    public GameObject[] row0 = new GameObject[4];
+    public GameObject[] row1 = new GameObject[4];
+    public GameObject[] row2 = new GameObject[4];
+    public GameObject[] row3 = new GameObject[4];
+    public List<GameObject> list = new List<GameObject>(49);
+    //public GameObject[] row4 = new GameObject[9];
+    //public GameObject[] row5 = new GameObject[9];
+    //public GameObject[] row6 = new GameObject[9];
+    //public GameObject[] row7 = new GameObject[9];
+    //public GameObject[] row8 = new GameObject[9];
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
