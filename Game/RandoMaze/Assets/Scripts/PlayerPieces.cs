d8aabcddc383c8a65ee1f343e22664eb525f0d03
Jenny Gia Linh Le <11010280@stud.hochschule-heidelberg.de>
2018-11-08 19:22:41 +0100

    turn-based with 4 Dummy Player

---
+++ Game/RandoMaze/Assets/Scripts/PlayerPieces.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPieces: MonoBehaviour {
+
+    public RandoMazeBoard boardTurnSystem;
+    public TurnClass turnClass;
+    public EndTurn endPlayerTurn;
+
+    public bool isTurn = false;     //bool to check if itz the Player's turn
+
+    private void Start()
+    {
+        boardTurnSystem = GameObject.Find("Board").GetComponent<RandoMazeBoard>();
+
+        //for each gameObject of Turnclass in the List of "Board"
+        foreach(TurnClass element in boardTurnSystem.playersGroup)
+        {
+            //if the gameObject Name matches the gameObject name in "Board" /check if gameObject is registered in "Board"
+                //add reference of element instance into turnclass
+            if (element.playerGameObject.name == gameObject.name)
+                turnClass = element;
+        }
+
+    }
+
+    private void Update()
+    {
+        //set value of isTurn equal to value that current Player holds
+        isTurn = turnClass.isTurn;
+
+        //if isTurn = true
+        if(isTurn)
+        {
+            //check if Button was pressed
+            if (endPlayerTurn.buttonPressed == true)
+            {
+                isTurn = false;     //set isTurn false again
+                turnClass.isTurn = isTurn;      //turnClass.isTurn = false
+                turnClass.wasTurnPrev = true;   //set the Players wasTurnPrev to true
+
+                endPlayerTurn.buttonPressed = false;    //change EndTurn Button was pressed to false again
+                
+            }
+        }
+    }
+}
---
+++ Game/RandoMaze/Assets/Scripts/RandoMazeBoard.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandoMazeBoard : MonoBehaviour {
+
+    //List of Players
+    public List<TurnClass> playersGroup;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //Reset turns in the beginning
+        ResetTurns();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        UpdateTurns();
+	}
+
+    //reset turn to Beginning
+        //Player1 starts
+        //rest didn't have their turn yet in this round
+    private void ResetTurns()
+    {
+        for(int i = 0; i < playersGroup.Count; i++)     //go through each player one by one
+        {
+            if(i == 0)  //start with first player
+            {
+                playersGroup[i].isTurn = true;
+                playersGroup[i].wasTurnPrev = false;
+            }
+            else        //rest set both false
+            {
+                playersGroup[i].isTurn = false;
+                playersGroup[i].wasTurnPrev = false;
+            }
+        }
+    }
+
+    //will be called in Update()
+    //Update Player Turns
+        //set current Player to isTurn = true
+        //set previous Player 
+    private void UpdateTurns()
+    {
+        for(int i = 0; i < playersGroup.Count; i++) //go through each player
+        {
+            //if Player didn't have his turn yet
+                //set isTurn to true
+                //break the loop so it doesn't get resetted
+            if (!playersGroup[i].wasTurnPrev)   
+            {
+                playersGroup[i].isTurn = true;
+                break;
+            }
+                //if iteration = amount of players And the last Player had his turn
+                    //reset the turns -> new round
+            else if (i == playersGroup.Count - 1 && playersGroup[i].wasTurnPrev)
+                ResetTurns();
+        }
+    }
+}
+
+    //needed for List
+[System.Serializable]
+public class TurnClass
+{
+    public GameObject playerGameObject;
+    public bool isTurn = false;
+    public bool wasTurnPrev = false;
+}
