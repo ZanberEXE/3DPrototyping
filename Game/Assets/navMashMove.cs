69b9a50b3edaa120facb66109916b5a37000da9c
Dr. Rabbit <huber.andreas123@gmail.com>
2018-11-13 13:54:32 +0100

    treasure started

--- Game/Assets/navMashMove.cs
+++ Game/Assets/navMashMove.cs
@@ -8,7 +8,8 @@ public class navMashMove : MonoBehaviour
 {
 
     UnityEngine.AI.NavMeshAgent agent;
-    public Vector3 targetPos;
+    Vector3 targetPos;
+    bool outOfPlane = true;
     public bool moving2 = false;
 	// Use this for initialization
 	void Start ()
@@ -19,24 +20,31 @@ public class navMashMove : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-        if (Input.GetMouseButton(0))
+        MouseDetect();
+        //MouseClickCheck();
+        if (outOfPlane == true)
         {
-            Plane plane = new Plane(Vector3.up, transform.position);
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            float point = 0f;
 
-            if (plane.Raycast(ray, out point))
+
+            if (Input.GetMouseButton(0))
             {
-                targetPos = ray.GetPoint(point);
-            }
-            if (moving2 == true)
-            {
+                Plane plane = new Plane(Vector3.up, transform.position);
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                float point = 10.0f;
+
+                if (plane.Raycast(ray, out point))
+                {
+                    targetPos = ray.GetPoint(point);
+                }
+                if (moving2 == true)
+                {
 
 
-            Move();
+                    Move();
+                }
             }
+
         }
-
         
 	}
 
@@ -44,5 +52,22 @@ public class navMashMove : MonoBehaviour
     {
         agent.SetDestination(targetPos);
         //Debug.DrawLine(transform.position, targetPos, Color.black);
+    }
+
+    void MouseClickCheck()
+    {
+        if (Input.mousePosition.x < 9.5 && Input.mousePosition.z < 3.3)
+        {
+            outOfPlane = false;
+        }
+        else
+        {
+            outOfPlane = true;
+        }
+    }
+
+    void MouseDetect()
+    {
+        Debug.Log(targetPos);
     }
 }
