edcdf54b832e48a9d0f71d08cde7686334df7fde
Jenny Gia Linh Le <11010280@stud.hochschule-heidelberg.de>
2018-11-16 00:53:30 +0100

    Alle Menus + Buttons implementiert

---
+++ Game/RandoMaze/Assets/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OpenOptions()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void OpenCredits()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
